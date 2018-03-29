4167e8f393f4f1d2f1988ca1a992d3d519adc325
Pablo Martinez <pablomzblasco@gmail.com>
2018-03-29 20:38:59 +0200

    Added Gradle

---
+++ src/main/java/ORM/Cancioneslista.java
@@ -0,0 +1,68 @@
+package ORM;
+
+import javax.persistence.*;
+import java.sql.Date;
+import java.util.Objects;
+
+@Entity
+public class Cancioneslista {
+    private int idCancLista;
+    private Date fechaIntroduccion;
+    private Listarep listarepByListaRep;
+    private Cancion cancionByIdCancion;
+
+    @Id
+    @Column(name = "idCancLista")
+    public int getIdCancLista() {
+        return idCancLista;
+    }
+
+    public void setIdCancLista(int idCancLista) {
+        this.idCancLista = idCancLista;
+    }
+
+    @Basic
+    @Column(name = "fechaIntroduccion")
+    public Date getFechaIntroduccion() {
+        return fechaIntroduccion;
+    }
+
+    public void setFechaIntroduccion(Date fechaIntroduccion) {
+        this.fechaIntroduccion = fechaIntroduccion;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cancioneslista that = (Cancioneslista) o;
+        return idCancLista == that.idCancLista &&
+                Objects.equals(fechaIntroduccion, that.fechaIntroduccion);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(idCancLista, fechaIntroduccion);
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "listaRep", referencedColumnName = "idLista", nullable = false)
+    public Listarep getListarepByListaRep() {
+        return listarepByListaRep;
+    }
+
+    public void setListarepByListaRep(Listarep listarepByListaRep) {
+        this.listarepByListaRep = listarepByListaRep;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "idCancion", referencedColumnName = "idCancion", nullable = false)
+    public Cancion getCancionByIdCancion() {
+        return cancionByIdCancion;
+    }
+
+    public void setCancionByIdCancion(Cancion cancionByIdCancion) {
+        this.cancionByIdCancion = cancionByIdCancion;
+    }
+}
---
+++ src/main/java/ORM/Usuario.java
@@ -0,0 +1,178 @@
+package ORM;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+public class Usuario {
+    private String idUser;
+    private String nomAp;
+    private String email;
+    private String conexion;
+    private int ultRep;
+    private String contrasenya;
+    private boolean publico;
+    private Collection<Album> albumsByIdUser;
+    private Collection<Cancion> cancionsByIdUser;
+    private Collection<Comentario> comentariosByIdUser;
+    private Collection<Listarep> listarepsByIdUser;
+    private Collection<Suscribir> suscribirsByIdUser;
+    private Collection<Suscribir> suscribirsByIdUser_0;
+
+    public Usuario() {}
+
+    public Usuario(String idUser, String nomAp, String email, String contrasenya) {
+        this.idUser = idUser;
+        this.nomAp = nomAp;
+        this.email = email;
+        this.conexion = "desconectado";
+        this.ultRep = 0;
+        this.contrasenya = contrasenya;
+        this.publico = false;
+    }
+
+    @Id
+    @Column(name = "idUser")
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    @Basic
+    @Column(name = "NomAp")
+    public String getNomAp() {
+        return nomAp;
+    }
+
+    public void setNomAp(String nomAp) {
+        this.nomAp = nomAp;
+    }
+
+    @Basic
+    @Column(name = "Email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Basic
+    @Column(name = "Conexion")
+    public String getConexion() {
+        return conexion;
+    }
+
+    public void setConexion(String conexion) {
+        this.conexion = conexion;
+    }
+
+    @Basic
+    @Column(name = "UltRep")
+    public int getUltRep() {
+        return ultRep;
+    }
+
+    public void setUltRep(int ultRep) {
+        this.ultRep = ultRep;
+    }
+
+    @Basic
+    @Column(name = "Contrasenya")
+    public String getContrasenya() {
+        return contrasenya;
+    }
+
+    public void setContrasenya(String contrasenya) {
+        this.contrasenya = contrasenya;
+    }
+
+    @Basic
+    @Column(name = "Publico")
+    public boolean isPublico() {
+        return publico;
+    }
+
+    public void setPublico(boolean publico) {
+        this.publico = publico;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return ultRep == usuario.ultRep &&
+                publico == usuario.publico &&
+                Objects.equals(idUser, usuario.idUser) &&
+                Objects.equals(nomAp, usuario.nomAp) &&
+                Objects.equals(email, usuario.email) &&
+                Objects.equals(conexion, usuario.conexion) &&
+                Objects.equals(contrasenya, usuario.contrasenya);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(idUser, nomAp, email, conexion, ultRep, contrasenya, publico);
+    }
+
+    @OneToMany(mappedBy = "usuarioByIdUser")
+    public Collection<Album> getAlbumsByIdUser() {
+        return albumsByIdUser;
+    }
+
+    public void setAlbumsByIdUser(Collection<Album> albumsByIdUser) {
+        this.albumsByIdUser = albumsByIdUser;
+    }
+
+    @OneToMany(mappedBy = "usuarioByIdUser")
+    public Collection<Cancion> getCancionsByIdUser() {
+        return cancionsByIdUser;
+    }
+
+    public void setCancionsByIdUser(Collection<Cancion> cancionsByIdUser) {
+        this.cancionsByIdUser = cancionsByIdUser;
+    }
+
+    @OneToMany(mappedBy = "usuarioByIdUser")
+    public Collection<Comentario> getComentariosByIdUser() {
+        return comentariosByIdUser;
+    }
+
+    public void setComentariosByIdUser(Collection<Comentario> comentariosByIdUser) {
+        this.comentariosByIdUser = comentariosByIdUser;
+    }
+
+    @OneToMany(mappedBy = "usuarioByIdUser")
+    public Collection<Listarep> getListarepsByIdUser() {
+        return listarepsByIdUser;
+    }
+
+    public void setListarepsByIdUser(Collection<Listarep> listarepsByIdUser) {
+        this.listarepsByIdUser = listarepsByIdUser;
+    }
+
+    @OneToMany(mappedBy = "usuarioByIdSuscrito")
+    public Collection<Suscribir> getSuscribirsByIdUser() {
+        return suscribirsByIdUser;
+    }
+
+    public void setSuscribirsByIdUser(Collection<Suscribir> suscribirsByIdUser) {
+        this.suscribirsByIdUser = suscribirsByIdUser;
+    }
+
+    @OneToMany(mappedBy = "usuarioByIdSuscriptor")
+    public Collection<Suscribir> getSuscribirsByIdUser_0() {
+        return suscribirsByIdUser_0;
+    }
+
+    public void setSuscribirsByIdUser_0(Collection<Suscribir> suscribirsByIdUser_0) {
+        this.suscribirsByIdUser_0 = suscribirsByIdUser_0;
+    }
+}
