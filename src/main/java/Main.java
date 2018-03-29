8ded34c1c54cd3b6b29e1fc5c363e73d91d956ff
Pablo Martinez <pablomzblasco@gmail.com>
2018-03-29 20:38:59 +0200

    Added Gradle

---
+++ src/main/java/Main.java
@@ -0,0 +1,82 @@
+import ORM.Usuario;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final Session session = getSession();
+        Scanner sc = new Scanner(System.in);
+        try {
+            /*
+            System.out.println("querying all the managed entities...");
+            final Metamodel metamodel = session.getSessionFactory().getMetamodel();
+            for (EntityType<?> entityType : metamodel.getEntities()) {
+                final String entityName = entityType.getName();
+                final Query query = session.createQuery("from " + entityName);
+                System.out.println("executing: " + query.getQueryString());
+                for (Object o : query.list()) {
+                    System.out.println("  " + o);
+                }
+            }
+            */
+
+                Usuario pablo = new Usuario();
+
+                pablo.setIdUser("pablo");
+                pablo.setNomAp("Pablo");
+                pablo.setContrasenya(" ");
+                pablo.setUltRep(10);
+                pablo.setPublico(false);
+                pablo.setConexion("conectado");
+                pablo.setEmail("email");
+
+                System.out.println(pablo);
+
+                session.beginTransaction();
+                session.save( pablo );
+                session.getTransaction().commit();
+
+                /*
+                System.out.println("Enter your username: ");
+                Scanner sc = new Scanner(System.in);
+                String username = sc.nextLine();
+                System.out.println("Your username is " + username);
+
+
+                session.beginTransaction();
+                session.save( new Usuario(  ) );
+                session.getTransaction().commit();
+                session.close();
+                */
+
+        } finally {
+            session.close();
+            sc.close();
+        }
+    }
+
+
+
+}
---
+++ src/main/java/ORM/Listarep.java
@@ -0,0 +1,79 @@
+package ORM;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Objects;
+
+@Entity
+public class Listarep {
+    private int idLista;
+    private String nombre;
+    private int numElementos;
+    private Collection<Cancioneslista> cancioneslistasByIdLista;
+    private Usuario usuarioByIdUser;
+
+    @Id
+    @Column(name = "idLista")
+    public int getIdLista() {
+        return idLista;
+    }
+
+    public void setIdLista(int idLista) {
+        this.idLista = idLista;
+    }
+
+    @Basic
+    @Column(name = "nombre")
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Basic
+    @Column(name = "numElementos")
+    public int getNumElementos() {
+        return numElementos;
+    }
+
+    public void setNumElementos(int numElementos) {
+        this.numElementos = numElementos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Listarep listarep = (Listarep) o;
+        return idLista == listarep.idLista &&
+                numElementos == listarep.numElementos &&
+                Objects.equals(nombre, listarep.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(idLista, nombre, numElementos);
+    }
+
+    @OneToMany(mappedBy = "listarepByListaRep")
+    public Collection<Cancioneslista> getCancioneslistasByIdLista() {
+        return cancioneslistasByIdLista;
+    }
+
+    public void setCancioneslistasByIdLista(Collection<Cancioneslista> cancioneslistasByIdLista) {
+        this.cancioneslistasByIdLista = cancioneslistasByIdLista;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "idUser", referencedColumnName = "idUser", nullable = false)
+    public Usuario getUsuarioByIdUser() {
+        return usuarioByIdUser;
+    }
+
+    public void setUsuarioByIdUser(Usuario usuarioByIdUser) {
+        this.usuarioByIdUser = usuarioByIdUser;
+    }
+}
