635068d10208ee000264c67903a4e1b087688348
Miguel Moniente <msmoniente@gmail.com>
2018-05-01 14:00:15 +0200

    Generado script de compilación y despliegue con descarga automática del servidor. Instrucciones en el README.md.

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Backend
+
+## Compilación y despliegue
+#### Linux / MacOS
+Dependencias:  
+- gradle  
+- wget
+- mysql (todavia no)
+
+`sh deploy.sh build`  
+Descarga automáticamente la versión establecida de Apache Tomcat y configura y despliega el proyecto.  
+Si el servidor ya está, solo compila y despliega.  
+
+`sh deploy.sh clean`  
+Detiene el servidor si está funcionando y elimina todos los archivos del sistema.  
+
+
+ 
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,47 @@
 #!/bin/sh
+
+
+#version="8.5.30"
+#short_ver="8"
+version="9.0.7"
+short_ver="9"
+dowload()
+{
+wget "http://apache.rediris.es/tomcat/tomcat-$short_ver/v$version/bin/apache-tomcat-$version.tar.gz"
+mkdir tomcat
+tar -xvzf apache-tomcat-$version.tar.gz
+mv apache-tomcat-$version/* tomcat/.
+rmdir apache-tomcat-$version
+rm apache-tomcat-$version.tar.gz
+}
+
+build()
+{
 gradle build
-sh ~/Desktop/apache-tomcat-9.0.7/bin/shutdown.sh
-rm -rf ~/Desktop/apache-tomcat-9.0.7/webapps/wolf*
-cp -r build/libs/wolfsound.war ~/Desktop/apache-tomcat-9.0.7/webapps/.
-sh ~/Desktop/apache-tomcat-9.0.7/bin/startup.sh
+sh ./tomcat/bin/shutdown.sh
+rm -rf ./tomcat/webapps/wolf*
+cp -r build/libs/wolfsound.war ./tomcat/webapps/.
+sh ./tomcat/bin/startup.sh
+}
+
+clean()
+{
+    sh ./tomcat/bin/shutdown.sh
+    rm -r tomcat*
+}
+
+case $1 in
+    build)
+        if [ -d tomcat ]; then
+            build
+        else
+            dowload
+            build
+        fi
+        ;;
+    clean)
+        clean
+        ;;
+esac
+
+
