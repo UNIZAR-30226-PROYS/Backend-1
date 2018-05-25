e83817e057adfb7ee08001fe03f9571c74580f6b
Ubaldo Ramón <ubi1616@gmail.com>
2018-05-25 12:29:02 +0200

    Creada funcion para cambiar nombre a un fichero.

---
+++ src/main/java/controller/changeFileName.java
@@ -0,0 +1,27 @@
+package main.java.controller;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.*;
+import java.nio.file.StandardCopyOption;
+
+
+public class changeFileName {
+
+
+    //Old name ruta antigua del fichero, newName ruta nueva.
+    public static void rename(String oldName, String newName)throws Exception {
+        File from = new File(oldName);
+        File to = new File(newName);
+        try {
+            Files.move(from.toPath(),to.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+        catch (Exception e ){throw new Exception("no se ha podido mover el fichero:"+e.getMessage());}
+        }
+
+
+
+    }
+
+}
