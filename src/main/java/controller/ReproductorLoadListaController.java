479612ad7a69860602e05a659db98756f2527192
Ubaldo Ramón <ubi1616@gmail.com>
2018-05-28 11:05:49 +0200

    Añadido servlet para cargar una lista en el reproductor.

---
+++ src/main/java/controller/ReproductorLoadListaController.java
@@ -0,0 +1,48 @@
+package main.java.controller;
+
+import main.java.model.Cancion;
+import main.java.model.Cancioneslista;
+import main.java.model.Listarep;
+import main.java.model.Usuario;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/Reproductor_lista_v2", name = "Reproductor_lista_v2_controller")
+public class ReproductorLoadListaController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+        List<Cancion> canciones = (List<Cancion>) session.getAttribute("canciones");
+        String text = "";
+        response.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
+        response.setCharacterEncoding("UTF-8"); // You want world domination, huh?
+
+
+        try {
+            Integer i =0;
+            Integer max_num_historioal =  Integer.parseInt( request.getParameter("max_num_canciones") );
+            Usuario u = (Usuario) session.getAttribute("username");
+            for (Cancion cancion : canciones){
+                text = text+cancion.getNombre()+",";
+                text = text+cancion.getIdCancion()+",";
+                if ( i<max_num_historioal ) i++;
+                else { break; }
+            }
+
+            response.getWriter().write(text);       // Write response body.
+
+        } catch (Exception e) {
+            response.getWriter().write("Ninguna cancion");       // Write response body.
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
